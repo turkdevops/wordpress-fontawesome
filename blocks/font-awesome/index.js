7652635fbcc9a14ac3e49418e75e8941a1c5f160
Mike Wilkerson <mwilkerson@gmail.com>
2019-01-09 21:37:43 -0600

    checkpoint

--- blocks/font-awesome/index.js
+++ blocks/font-awesome/index.js
@@ -1,14 +1,18 @@
 ( function( wp ) {
-	/**
-	 * Registers a new block provided a unique name and an object defining its behavior.
-	 * @see https://github.com/WordPress/gutenberg/tree/master/blocks#api
-	 */
-	const registerBlockType = wp.blocks.registerBlockType
-	/**
-	 * Returns a new element of given type. Element is an abstraction layer atop React.
-	 * @see https://github.com/WordPress/gutenberg/tree/master/element#element
-	 */
-	const el = wp.element.createElement
+  const {
+	  BlockControls,
+    registerBlockType
+	} =  wp.blocks
+
+	const {
+    createElement,
+    Fragment
+  } = wp.element
+
+  const {
+    SVG,
+    Path
+  } = wp.components
 	/**
 	 * Retrieves the translation of text.
 	 * @see https://github.com/WordPress/gutenberg/tree/master/i18n#api
@@ -32,7 +36,7 @@
 		 */
 		category: 'widgets',
 
-    icon: el(
+    icon: createElement(
       'svg',
       {
         className: "svg-inline--fa fa-font-awesome fa-w-14",
@@ -40,7 +44,7 @@
         viewBox: "0 0 448 512",
 
       },
-      el(
+      createElement(
         'path',
         {
           fill: "currentColor",
@@ -132,6 +136,54 @@
       return <i className={`${ prefix } fa-${ icon }`} icon={ icon } prefix={ prefix }></i>
 		}
 	})
+
+  const { registerFormatType } = window.wp.richText
+
+  const type = 'font-awesome/icon'
+
+  const { RichTextToolbarButton } = window.wp.editor
+
+  const title = __('Font Awesome Icon')
+
+  registerFormatType(type, {
+    title,
+    tagName: 'i',
+    className: 'font-awesome-format-type',
+    attributes: {
+      icon: 'star'
+    },
+    edit ({ isActive, value, onChange }) {
+
+      // console.log(`DEBUG registerFormatType edit: isActive = ${isActive}, value:`, value)
+      const handleClick = () => {
+        //onChange(toggleFormat(value, { type }))
+        console.log("DEBUG: handleClick")
+      }
+
+      return (
+        <Fragment>
+          <RichTextToolbarButton
+            title={ title }
+            isActive={ isActive }
+            shortcutType={ 'primary' }
+            onClick={ handleClick }
+            className={ `toolbar-button-with-text toolbar-button__advanced-${name}` }
+            icon={
+              <SVG
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className="svg-inline--fa fa-font-awesome fa-w-14">
+                <Path
+                  fill="currentColor"
+                  d="M397.8 32H50.2C22.7 32 0 54.7 0 82.2v347.6C0 457.3 22.7 480 50.2 480h347.6c27.5 0 50.2-22.7 50.2-50.2V82.2c0-27.5-22.7-50.2-50.2-50.2zm-45.4 284.3c0 4.2-3.6 6-7.8 7.8-16.7 7.2-34.6 13.7-53.8 13.7-26.9 0-39.4-16.7-71.7-16.7-23.3 0-47.8 8.4-67.5 17.3-1.2.6-2.4.6-3.6 1.2V385c0 1.8 0 3.6-.6 4.8v1.2c-2.4 8.4-10.2 14.3-19.1 14.3-11.3 0-20.3-9-20.3-20.3V166.4c-7.8-6-13.1-15.5-13.1-26.3 0-18.5 14.9-33.5 33.5-33.5 18.5 0 33.5 14.9 33.5 33.5 0 10.8-4.8 20.3-13.1 26.3v18.5c1.8-.6 3.6-1.2 5.4-2.4 18.5-7.8 40.6-14.3 61.5-14.3 22.7 0 40.6 6 60.9 13.7 4.2 1.8 8.4 2.4 13.1 2.4 22.7 0 47.8-16.1 53.8-16.1 4.8 0 9 3.6 9 7.8v140.3z"
+                />
+              </SVG>
+            }
+          />
+        </Fragment>
+      )
+    }
+  })
 } )(
 	window.wp
 )
