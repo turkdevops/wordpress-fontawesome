a55f59da7f938d34090251d8b43d30c264b05ab3
Mike Wilkerson <mwilkerson@gmail.com>
2020-04-29 15:46:38 -0700

    don't use shadow DOM for custom element

--- blocks/fa-icon/index.js
+++ blocks/fa-icon/index.js
@@ -1,4 +1,4 @@
-import { LitElement, html, unsafeCSS } from 'lit-element'
+import { LitElement, html } from 'lit-element'
 import { dom } from '@fortawesome/fontawesome-svg-core'
 
 export class FontAwesomeIcon extends LitElement {
@@ -8,17 +8,23 @@ export class FontAwesomeIcon extends LitElement {
    }
   }
 
-  static get styles() {
-    return [
-      unsafeCSS(dom.css())
-    ]
+  // TODO: consider what errors to which this might be prone,
+  // inserting the Css in the construction of each component.
+  constructor() {
+    super()
+    dom.insertCss(dom.css())
+  }
+
+  createRenderRoot() {
+    // This will result in no Shadow DOM being used for this custom element.
+    return this
   }
 
   render() {
     const {
       icon
     } = JSON.parse(this.json)
-    return html`<svg class="svg-inline--fa" viewBox="0 0 ${icon[0]} ${icon[1]}"><path d="${icon[4]}"></path></svg>`
+    return html`<svg class="svg-inline--fa" viewBox="0 0 ${icon[0]} ${icon[1]}"><path fill="currentColor" d="${icon[4]}"></path></svg>`
   }
 }
 
